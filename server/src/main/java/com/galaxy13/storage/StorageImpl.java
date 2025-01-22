2fa95a3dc1252facaf982e9ef854d5cb3220a02a
Galaxy13 <gen.hort54@gmail.com>
2025-01-22 21:39:13 +0300

    2025.01.22: Storage impl re-done

--- server/src/main/java/com/galaxy13/storage/StorageImpl.java
+++ server/src/main/java/com/galaxy13/storage/StorageImpl.java
@@ -1,9 +1,38 @@
 package com.galaxy13.storage;
 
-import java.util.HashMap;
+import java.lang.ref.WeakReference;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class StorageImpl extends AbstractStorage implements Storage {
+    protected Map<String, WeakReference<Value>> storage;
+
     public StorageImpl(int capacity) {
-        this.storage = new HashMap<>(capacity);
+        this.storage = new ConcurrentHashMap<>(capacity);
+    }
+
+    @Override
+    public Optional<Value> get(String key) {
+        WeakReference<Value> weakReference = storage.get(key);
+        if (weakReference != null) {
+            if (weakReference.get() != null) {
+                return Optional.ofNullable(weakReference.get());
+            } else {
+                storage.remove(key);
+            }
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public Value put(String key, Value value) {
+        storage.put(key, new WeakReference<>(value));
+        return value;
+    }
+
+    @Override
+    public Optional<Value> remove(String key) {
+        return Optional.ofNullable(storage.remove(key)).map(WeakReference::get);
     }
 }
